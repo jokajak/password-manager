f4231ae4c60849f3f828ea428c1f01babd030858
jokajak <jokajak@gmail.com>
2018-05-20 17:25:54 -0400

    refactor how the flask app is run
    created a manage.py function that has more capabilities. Also define a
    requirements.txt

--- backend/flask/src/run.py
+++ backend/flask/src/run.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from clipperz import app, db
 
-
 def main():
     db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
 
 if __name__ == "__main__":
     main()
